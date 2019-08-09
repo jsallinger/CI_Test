68d56f3163fa3b64c082d841183053f4a4b21cb1
Jason Sallinger <jsallinger@gmail.com>
2019-08-09 09:35:49 -0400

    New Widget Files

---
+++ TeamServiceWebApp/Controllers/WidgetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamServiceWebApp.Models;
+
+namespace TeamServiceWebApp.Controllers
+{
+    public class WidgetController
+    {
+        public bool IncrementItemNumber(WidgetModel widget)
+        {
+            bool isIncremented = false;
+
+            int startItemNumber = widget.ItemNumber;
+            widget.ItemNumber++;
+            if (startItemNumber < widget.ItemNumber)
+                isIncremented = true;
+
+            return isIncremented;
+        }
+    }
+}
---
+++ TeamServiceWebApp/Models/WidgetModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamServiceWebApp.Models
+{
+    public class WidgetModel
+    {
+        public WidgetModel()
+        {
+            Description = string.Empty;
+            ItemNumber = 0;
+            IsActive = true;
+        }
+
+        public string Description { get; set; }
+        public int ItemNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
